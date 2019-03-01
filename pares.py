9ba41d8fe2dfb4fa3848f41484b8005778cedebf
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-01 13:18:34 -0500

    Ejercicio dos resuelto

--- pares.py
+++ pares.py
@@ -1,6 +1,19 @@
-a = int(input("Ingrese un número: "))
-if (a%2) == 0:
-    print("El número es par")
-else:
-    print("El número es impar")
+numeros = []
+pares = []
+impares= []
+a = int(input("Ingrese un numero: "))
+while (a%2)==0 :
+        for b in range(3) :
+            print("Número par")
+            a = int(input("Ingrese un numero: "))
+            par = a
+            pares.append (par)
+            break
+if a%2 != 0:
+    impar = a
+    impares.append (impar)
+    print ("Número impar")
 
+for c in range (1):
+    print ("Hay ",len(pares), "número(s) par(es): ", pares)
+    print ("Hay ",len(impares), "número(s) impar(es): ", impares)
