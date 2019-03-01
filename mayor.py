2dfa89281a4c8943e2b27f77e331b7b5c1e2e050
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2019-03-01 11:53:34 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,5 +1,25 @@
-a = int(input("Ingrese un numero: "))
-if a > 0:
-    print("Número mayor que cero")
-else:
-    print ("Número menor o igual a cero")
+numeros = []
+mayores = []
+menores = []
+ceros = []
+for datos in range(3) :
+    a = int(input("Ingrese un numero: "))
+    numeros.append (a)
+
+    if a > 0:
+        mayor = a
+        mayores.append (mayor)
+        print("Número mayor que cero")
+    elif a < 0:
+        menor = a
+        menores.append (menor)
+        print ("Número menor a cero")
+    else :
+        cero = a
+        ceros.append (cero)
+        print ("El número es cero")
+
+for datos in range (1):
+    print ("Hay ",len(mayores), "número(s) mayor(es) que cero: ", mayores)
+    print ("Hay ",len(menores), "número(s) menor(es) que cero: ", menores)
+    print ("Hay ",len(ceros), "ceros: ", ceros)
